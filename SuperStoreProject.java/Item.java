040f968df84b4bb41bb4bd5b6c9061516639d58f
Skylar Fraser <114590754+skylarfraser@users.noreply.github.com>
2023-04-26 10:14:06 -0400

    Add files via upload
    Here I am committing changes for the most current status of my game.

--- SuperStoreProject.java/Item.java
+++ SuperStoreProject.java/Item.java
@@ -1,96 +1,119 @@
 import java.util.Random;
-import java.util.ArrayList;
 
 
 public class Item {
-
 //Fields
 String label; 
 Long price; 
 int count;
 int sale;
 int index;
-Random rand;
 
-//Constructor
-//NOTE: MAYBE ADD A NUMBER FIELD THAT NUMBERS THE ITEM BASED ON ITS INDEX
-//EX: [0] Bag
+
+
 Item(int index, String label, Long price, int count){
     this.label = label;
     this.price = price;
     this.count = count;
     this.index = index;
+
 }
+ 
 
-public void addSale (){
-    Random rand = new Random ();
-
-    rand.nextInt(0,2);
-
-    if (rand.nextInt() == 0){
-        fashdisc();
-    }
-    else if (rand.nextInt() == 1){
-        estatedisc();
-
-    }
-    else if (rand.nextInt() == 2){
-        miscdisc();
-    }
-
+//The standard discount (which does not do anything except return the price for now because
+//each subclass can override this function.)
+public Long mainDiscount(Random rand) {
+    return price;
 }
 
 
 
-public void fashdisc(){
-    Random rand = new Random ();
-
-    int fashmin = 1200000;
-    int fashmax = 1800000;
-    int fashsale = rand.nextInt(fashmin, fashmax);
-    for (int i = 0; i < 3;  i++){
-        this.price -= fashsale;
-    }
-}
-
-public void estatedisc(){
-    int estatemin = 8000000;
-    int estatemax = 9000000;
-    int estatesale = rand.nextInt(estatemin, estatemax);
-    for (int i = 3; i < 6; i++){
-        this.price -= estatesale;
-    }
-}
-
-public void miscdisc(){
-    int miscmin = 75000000;
-    int miscmax = 100000000;
-    int miscsale = rand.nextInt(miscmin, miscmax);
-    for (int i = 6; i < 11;  i++){
-        this.price -= miscsale;
-    }
-}
-
-
- public void blackFridaySale(ArrayList <Item> fashion, ArrayList <Item> realestate, ArrayList <Item> misc){
+/* public void blackFridaySale(ArrayList <Item> items){
     for (int i = 0; i < 3; i++){
-        long fhalf = fashion.get(i).price ;
+        long fhalf = items.get(i).price ;
         this.price -= fhalf;
     }
 
     for (int i = 3; i < 6; i++){
-        long rhalf = realestate.get(i).price;
+        long rhalf = items.get(i).price;
         this.price -= rhalf;
     }
 
-    for(int i = 6; i < 11; i++){
-        long mhalf = misc.get(i).price;
+    for(int i = 6; i < 10; i++){
+        long mhalf = items.get(i).price;
         this.price -= mhalf;
+    }
+} */
+
+
+}
+
+
+
+class Fashion extends Item{
+    public Fashion (int index, String label, Long price, int count){
+        super(index, label, price, count);
+    }
+
+    @Override 
+    public Long mainDiscount (Random rand){
+        Long fashmin = 1200000l;
+        Long fashmax = 1800000l;
+        long fashsale = rand.nextLong(fashmin, fashmax);
+        for (int i = 0; i < 3;  i++){
+            this.price -= fashsale;
+        }
+        return price;
     }
 }
 
 
+
+
+class RealEstate extends Item{
+    public RealEstate (int index, String label, Long price, int count){
+        super(index, label, price, count);
+
+    }
+
+    @Override 
+    public Long mainDiscount (Random rand){
+        Long estatemin = 8000000l;
+        Long estatemax = 9000000l;
+        long estatesale = rand.nextLong(estatemin, estatemax);
+        for (int i = 3; i < 6; i++){
+            this.price -= estatesale; 
+        }
+        return price;
+    }
 }
+
+
+class Misc extends Item{
+
+    public Misc (int index, String label, Long price, int count){
+        super(index, label, price, count);
+    }
+
+    @Override 
+    public Long mainDiscount (Random rand){
+        Long miscmin = 75000000l;
+        Long miscmax = 100000000l;
+        long miscsale = rand.nextLong(miscmin, miscmax);
+        for (int i = 6; i < 10;  i++){
+            this.price -= miscsale;
+        }
+        return price;
+    }
+}
+
+
+
+
+
+
+
+
 
 
 
--- SuperStoreProject.java/SkylarSuperStore.java
+++ SuperStoreProject.java/SkylarSuperStore.java
@@ -1,94 +1,119 @@
 import java.util.Scanner;
+import java.util.Random;
 
 //Skylar Fraser 10
 //Super Store Game
 //B Period
 
+//QUESTIONS: 
+//1. Ask about the add sale issue (says that i cannot make a static reference
+//to a non static method- but when i make the main discount method static
+// it just adds more errors). 
 
-public class SkylarSuperStore{
+public class SkylarSuperStore {
     static long wallet = 700000000;
-    static int trials;
-    static Scanner sc = new Scanner(System.in);
+    static int trials = 0;
     static int index;
-
+    private static Item fashitem;
+    private static Item realitem;
+    private static Item miscitem;
+    static Scanner sc = new Scanner(System.in);
+    static Random rand = new Random();
 
     public static void main(String[] args) throws Exception {
 
-        System.out.println("After being the only one in London to survive the apocalypse (since you hid in the underground bunker of a bank), you now have access to the vault storing hundreds of millionaires’ money and it is up to you to spend all of the money before the state authorities find you.");
-        System.out.println("Below you will find a selection of finely-picked items from a very legal company in a very legal market!");
-    
+        System.out.println(
+                "After being the only one in London to survive the apocalypse (since you hid in the underground bunker of a bank), you now have access to the vault storing hundreds of millionaires’ money and it is up to you to spend all of the money before the state authorities find you.");
+        System.out.println(
+                "Below you will find a selection of finely-picked items from a very legal company in a very legal market!");
+
         Store.createItems();
         Store.displaystore();
 
+        addSale(rand);
+
         while (true) {
-            buyItem(sc, trials);
+            buyItem(sc, trials, fashitem, realitem, miscitem);
         }
-     
+
     }
- 
 
-    // public static void mainGame(Scanner sc){
-    //     System.out.println("In order to purchase an item, please type in its corresponding number on the list.");
-    //     buyItem (sc, trials);
+    public static void buyItem(Scanner sc, int trials, Item fashitem, Item realitem, Item miscitem) {
+        if (sc.hasNextInt()) {
+            trials++;
 
-    // }
-
-
-        public static void buyItem(Scanner sc, int trials){
-
-            if (sc.hasNextInt()){
-                trials++;
-
-                //Item = desired purchase of user (which corresponds to a specific number on the list)
-                index = sc.nextInt();
-                Item item = Store.getItemWithIndex(index);
-                if (index < 0 || index >= 10){
-                    System.out.println("This is an unidentable item. Please type in an valid number.");
-                    buyItem(sc, trials);
-                }
-                
-                wallet -= item.price;
-                item.count --; 
-                System.out.println("Wallet: " + wallet);
-                System.out.println();
-                Store.displaystore();
-
-                if (trials == 10 && wallet > 525000000){
-                    System.out.println("Unfortunately, you have only spent" + wallet + "dollars. You ran out of time and have been caught, meaning you lost the game. Feel free to try again!");
-                    buyItem(sc, trials);;
-                }
-        
-                //once the size of the array list is 10, but the user has spent more than
-                //the majority or greater than it, then the user has techincally not lost the game
-                else if (trials == 10 && wallet <= 525000000){
-                    System.out.println("Although you have run out of time, you successfuly spent the bare majoirity of the money so you were able to escape! Feel free to try again!");
-                    buyItem(sc, trials);
-                }
-        
-                //if the user spent all of the money in 10 or fewer turns, then they have won the game
-                else if (trials >= 10 && wallet <= 0){
-                    System.out.println("Congratulations! You were successfully able to spend all of the money! You won and you don't have to go to jail!");
-                    System.exit(0);
-
-
-
-
-         
-                }
-    
-            
-                
+            // Item = desired purchase of user (which corresponds to a specific number on
+            // the list)
+            index = sc.nextInt();
+            Item item = Store.getItemWithIndex(index);
+            if (index < 0 || index >= 10) {
+                System.out.println("This is an unidentable item. Please type in an valid number.");
+                buyItem(sc, trials, fashitem, realitem, miscitem);
             }
-            else {
-                sc.next(); 
-                System.out.println("The input is not a number. Please type in a valid number!");
-                buyItem(sc, trials);
-            }
-     
 
-    
+            wallet -= item.price;
+            item.count--;
+
+            System.out.println("Wallet: " + wallet);
+            System.out.println();
+            Store.displaystore();
+
+            if (trials == 10 && wallet > 525000000) {
+                System.out.println("Unfortunately, you have only spent" + wallet
+                        + "dollars. You ran out of time and have been caught, meaning you lost the game. Feel free to try again!");
+                buyItem(sc, trials, fashitem, realitem, miscitem);
+                ;
+            }
+
+            // once the size of the array list is 10, but the user has spent more than
+            // the majority or greater than it, then the user has techincally not lost the
+            // game
+            else if (trials == 10 && wallet <= 525000000) {
+                System.out.println(
+                        "Although you have run out of time, you successfuly spent the bare majoirity of the money so you were able to escape! Feel free to try again!");
+                buyItem(sc, trials, fashitem, realitem, miscitem);
+            }
+
+            // if the user spent all of the money in 10 or fewer turns, then they have won
+            // the game
+            else if (trials <= 10 && wallet == 0) {
+                System.out.println(
+                        "Congratulations! You were successfully able to spend all of the money! You won and you don't have to go to jail!");
+                System.exit(0);
+
+            }
+
+        } else {
+            sc.next();
+            System.out.println("The input is not a number. Please type in a valid number!");
+            buyItem(sc, trials, fashitem, realitem, miscitem);
+        }
+
+    }
+
+    public static void addSale(Random rand) {
+        rand.nextInt(0, 3); // exclusive of max
+
+        if (rand.nextInt() == 0) {
+            // put all fashion items on sale
+            for (Item i : Store.getFashItems()) {
+                i.mainDiscount(rand);
+            }
+
+
+        } else if (rand.nextInt() == 1) {
+            // put all realestate items on sale
+            for (Item i : Store.getRealItems()) {
+                i.mainDiscount(rand);
+            }
+
+
+        } else if (rand.nextInt() == 2) {
+            // put all misc items on sale
+            for (Item i : Store.getMiscItems()) {
+                i.mainDiscount(rand);
+            }
+        }
+    }
 
 }
-
-}
-
